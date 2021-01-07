a109883b8e94e5d8cf94eadf2b7e7992bc0aefdf
dudwns9331 <dudwns1045@naver.com>
2021-01-07 18:09:46 +0900

    2020-01-07, Solved.ac Bronze3

---
+++ BaekJoon/Solved_Bronze3/2490.py
@@ -0,0 +1,41 @@
+# 윷놀이
+
+"""
+2021-01-07 오후 4:51
+안영준
+
+문제
+우리나라 고유의 윷놀이는 네 개의 윷짝을 던져서 배(0)와 등(1)이 나오는 숫자를
+세어 도, 개, 걸, 윷, 모를 결정한다. 네 개 윷짝을 던져서 나온 각 윷짝의 배 혹은
+등 정보가 주어질 때 도(배 한 개, 등 세 개), 개(배 두 개, 등 두 개), 걸(배 세 개, 등 한 개),
+윷(배 네 개), 모(등 네 개) 중 어떤 것인지를 결정하는 프로그램을 작성하라.
+
+입력
+첫째 줄부터 셋째 줄까지 각 줄에 각각 한 번 던진 윷짝들의 상태를 나타내는 네 개의 정수(0 또는 1)가  빈칸을 사이에 두고 주어진다.
+
+출력
+첫째 줄부터 셋째 줄까지 한 줄에 하나씩 결과를  도는 A, 개는 B, 걸은 C, 윷은 D, 모는 E로 출력한다.
+
+"""
+
+
+def result(count):
+    if count == 0:
+        print("E")
+    elif count == 1:
+        print("A")
+    elif count == 2:
+        print("B")
+    elif count == 3:
+        print("C")
+    elif count == 4:
+        print("D")
+
+
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+list3 = list(map(int, input().split()))
+
+result(list1.count(0))
+result(list2.count(0))
+result(list3.count(0))

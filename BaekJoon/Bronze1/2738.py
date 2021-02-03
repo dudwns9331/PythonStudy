a5f0012f8de25e35820d1fe95a63e9c14a80e719
dudwns9331 <dudwns1045@naver.com>
2021-02-03 10:44:15 +0900

    2021-02-03, Solved.ac Bronze1

--- BaekJoon/Bronze1/2738.py
+++ BaekJoon/Bronze1/2738.py
@@ -18,9 +18,22 @@ N과 M은 100보다 작거나 같고, 행렬의 원소는 절댓값이 100보다
 """
 
 N, M = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(N)]
+
+a_result = list()
+b_result = list()
 for i in range(N):
-    b = list(map(int, input().split()))
-    for j in range(M):
-        print(a[i][j] + b[j], end=' ')
-    print()
+    a_result.append(list(map(int, input().split())))
+
+for j in range(N):
+    b_result.append(list(map(int, input().split())))
+
+c_result = []
+
+for k in range(N):
+    result = []
+    for x in range(M):
+        result.append(a_result[k][x] + b_result[k][x])
+    c_result.append(result)
+
+for i in c_result:
+    print(' '.join(map(str, i)))

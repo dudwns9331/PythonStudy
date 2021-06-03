f072769102d0b66abef53e128fabd298c520e17d
dudwns9331 <dudwns1045@naver.com>
2021-06-03 23:27:52 +0900

    2021-06-03, Solved.ac Silver2

---
+++ BaekJoon/Silver2/15353.py
@@ -0,0 +1,14 @@
+# 큰 수 A + B
+
+"""
+2021-06-03 오후 11:23
+안영준
+
+문제 : https://www.acmicpc.net/problem/15353
+"""
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+A, B = map(int, input().split())
+print(A + B)

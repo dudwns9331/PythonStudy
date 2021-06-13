cb1cfb284996473c7ca97cf9bf6488828aaa0a55
dudwns9331 <dudwns1045@naver.com>
2021-06-13 23:19:54 +0900

    2021-06-13, Solved.ac Silver2

---
+++ BaekJoon/Silver2/15965.py
@@ -0,0 +1,37 @@
+# K번째 소수
+
+"""
+2021-06-12 오후 10:52
+안영준
+
+문제 : https://www.acmicpc.net/problem/15965
+"""
+
+import math
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def isPrime(number: int):
+    if number == 1:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+        return True
+
+
+N = int(input())
+
+result = list()
+count = 1
+
+while len(result) != N:
+    if isPrime(count):
+        result.append(count)
+    count += 1
+
+print(result[-1])
+# asdfasdf
